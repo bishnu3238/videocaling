febddb341f8591ce6e5c8c48540fff1df2399de4
bishnu3238 <64030672+bishnu3238@users.noreply.github.com>
2023-03-30 15:18:44 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -8,30 +8,76 @@ const io = new Server(httpServer);
 
 
 
-// const app = require('http').createServer();
-// const io = require('socket.io')(app);
 
 const port = process.env.PORT || 3000;
 
-  
-// io.on('connection', (socket) => {
-//   console.log(`User connected: ${socket.id}`);
-  
-//   socket.on('disconnect', () => {
-//     console.log(`User disconnected: ${socket.id}`);
-//   });
-// });
+
+io.use((socket, next) => {
+  if (socket.handshake.query) {
+    let callerId = socket.handshake.query.callerId;
+    socket.user = callerId;
+    next();
+  }
+});
+
+
+
+
+ 
 io.on('connection', (socket) => {
   console.log(`User connected: ${socket.id}`);
+
+  socket.join(socket.user);
+
+  socket.on("makeCall", (data) => {
+    let calleeId = data.calleeId;
+    let sdpOffer = data.sdpOffer;
+
+    console.log(`Received answer from user ${data.callerId}: ${data.sdpOffer}`);
+
+    socket.to(calleeId).emit("newCall", {
+      callerId: socket.user,
+      sdpOffer: sdpOffer,
+    });
+  });
+
   // socket.on('offer', (offer) => {
   //   console.log(`Received offer from user ${socket.id}: ${offer}`);
   //   socket.broadcast.emit('offer', offer);
   // });
   
+
+  socket.on("answerCall", (data) => {
+    let callerId = data.callerId;
+    let sdpAnswer = data.sdpAnswer;
+
+    console.log(`Received answer from user ${data.callerId}: ${data.sdpAnswer}`);
+
+    socket.to(callerId).emit("callAnswered", {
+      callee: socket.user,
+      sdpAnswer: sdpAnswer,
+    });
+  });
+
+
   // socket.on('answer', (answer) => {
   //   console.log(`Received answer from user ${socket.id}: ${answer}`);
   //   socket.broadcast.emit('answer', answer);
   // });
+
+
+  
+  socket.on("IceCandidate", (data) => {
+    let calleeId = data.calleeId;
+    let iceCandidate = data.iceCandidate;
+
+    console.log(`Received ICE candidate from user ${socket.id}: ${data.iceCandidate} and [${data}]`);
+
+    socket.to(calleeId).emit("IceCandidate", {
+      sender: socket.user,
+      iceCandidate: iceCandidate,
+    });
+  });
   
   // socket.on('ice-candidate', (candidate) => {
   //   console.log(`Received ICE candidate from user ${socket.id}: ${candidate}`);
